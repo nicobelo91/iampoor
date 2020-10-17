1a6cb522c431ad43a7de6bf53adf21ea84393d3d
Nico Cobelo <ingcobelo@gmail.com>
2020-10-17 10:29:02 -0400

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by user183145 on 10/17/20.
+//  Copyright © 2020 Nico Cobelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
